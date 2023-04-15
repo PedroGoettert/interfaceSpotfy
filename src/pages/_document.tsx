472323428b13cec7ab7337e7b8e3751226b471a1
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2023-04-14 21:22:28 -0300

    Initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+export const metaData = {
+  title: 'Spotfy',
+  description: 'Listen to music'
+}
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-zinc-800 text-zinc-50'>
         <Main />
         <NextScript />
       </body>
